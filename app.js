b4f7af72b4fd04ba1d4f30ea9f5238e225e6aeee
shafi <shafia774@gmail.com>
2023-08-31 22:53:33 +0530

    setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+ res.send('hello world');
+});
+
+app.get('/api/courses' ,(req,res) => {
+    res.send([1,2,3]);
+   });
+
+
+app.listen(3000,()=> console.log('Listening on port port 3000..'))
+// app.post()
+// app.put()
+// app.delete()
